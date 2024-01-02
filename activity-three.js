da3f8042de659f6984565ddfcf06708c7826c8f6
Aaron Latham <aaron@Aarons-Mac-mini.local>
2024-01-02 16:35:22 +0000

    updated for zero index

--- activity-three.js
+++ activity-three.js
@@ -6,9 +6,9 @@ function calculateDays(birthday) {
     let days = Math.floor((birthDate - today) / (1000 * 60 * 60 * 24));
 
     return console.log(
-        days < 0 ? `Your birthday was ${Math.abs(days)} days ago.` : `Your birthday is in ${days} days.`
+        days < 0 ? `Your birthday was ${Math.abs(days) - 1} days ago.` : `Your birthday is in ${days + 1} days.`
     );
 }
 
 // Change this to your birthday to test the program
-calculateDays("2023-07-08"); // Must be in YYYY-MM-DD format - ISO 8601 i think?
+calculateDays("2024-07-08"); // Must be in YYYY-MM-DD format - ISO 8601 i think?

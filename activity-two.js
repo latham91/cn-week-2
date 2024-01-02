599d50e2d55ec3270141efa6f44da6f1f8a5f3d1
Aaron Latham <aaron@Aarons-Mac-mini.local>
2024-01-02 15:33:41 +0000

    refactored

--- activity-two.js
+++ activity-two.js
@@ -3,11 +3,14 @@
 let breakfast = "cereal";
 let lunch = "chicken";
 let dinner = "pizza";
+let reply = `Today I ate ${breakfast} for breakfast, ${lunch} for lunch and ${dinner} for dinner.`;
 
-console.log(`Today I ate ${breakfast} for breakfast, ${lunch} for lunch and ${dinner} for dinner.`);
+console.log(reply);
 
 breakfast = "toast";
 lunch = "soup";
 dinner = "pasta";
 
-console.log(`Tomorrow I will eat ${breakfast} for breakfast, ${lunch} for lunch and ${dinner} for dinner.`);
+reply = `Tomorrow I will eat ${breakfast} for breakfast, ${lunch} for lunch and ${dinner} for dinner.`;
+
+console.log(reply);

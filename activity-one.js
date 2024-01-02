76d8b8ffac6b18e6d6b246a0485e873c9203f4aa
Aaron Latham <aaron@Aarons-Mac-mini.local>
2024-01-02 14:44:02 +0000

    updated

--- activity-one.js
+++ activity-one.js
@@ -5,7 +5,3 @@ let age = 32;
 let favColor = "purple";
 
 console.log(`My name is ${name} and I am ${age} years old. My favourite color is ${favColor}`);
-
-// Create a program that stores what you eat today for breakfast, lunch and dinner.
-
-// Create a program to calculate the number of days to your birth date.

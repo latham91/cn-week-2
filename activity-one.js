12153f7759a4e8bda91459d9683856145b020ddd
Aaron Latham <aaron@Aarons-Mac-mini.local>
2024-01-02 13:55:55 +0000

    Completed first activity

---
+++ activity-one.js
@@ -0,0 +1,4 @@
+// Display the 8th character of the sentence in uppercase to the console.
+console.log("All Around The World".charAt(7).toUpperCase());
+console.log("All Around The World"[7].toUpperCase());
+console.log("All Around The World".slice(7, 8).toUpperCase());

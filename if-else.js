00c8de75e6ec55dca5b3695979e09fb9770ffa94
Aaron Latham <latham91@icloud.com>
2024-01-03 10:00:25 +0000

    Initial commit for today

---
+++ if-else.js
@@ -0,0 +1,11 @@
+//  Comparison Operators
+///////////////////////////
+
+//  == - equality operator
+//  === - strict equality operator
+//  != - not equal operator
+//  !== - strictly not equal operator
+//  < - less than operator
+//  > - greater than operator
+//  <= - less than or equal to operator
+//  >= - greater than or equal to operator

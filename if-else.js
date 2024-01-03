e654428d7f7cae770eea8b83290076ec07bef3d3
Aaron Latham <latham91@icloud.com>
2024-01-03 10:03:47 +0000

    Added logical operator comments

--- if-else.js
+++ if-else.js
@@ -9,3 +9,10 @@
 //  > - greater than operator
 //  <= - less than or equal to operator
 //  >= - greater than or equal to operator
+
+// Logical Operators
+///////////////////////////
+
+//  && - logical AND operator - both sides need to be true
+//  || - logical OR operator - one side needs to be true
+//  ! - logical NOT operator - reverses the boolean value
